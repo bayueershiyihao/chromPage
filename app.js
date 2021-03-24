f939070890d57394f5f4998bcdfd181e228949c6
bayueershiyihao <76773766+bayueershiyihao@users.noreply.github.com>
2021-03-24 21:05:56 +0900

    Add typing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const appItem = document.querySelectorAll(".app-item");
 const calculator = document.querySelector(".calculator");
 const button = document.querySelector(".canvas-buttons");
 const appCanvas = document.querySelector("canvas");
+
 function showing() {
   appItem.forEach((e) => {
     e.addEventListener("click", (item) => {
@@ -10,13 +11,21 @@ function showing() {
         appCanvas.classList.toggle("showing");
         button.classList.toggle("showing");
 
-        if (!calculator.classList.contains("showing2")) {
-          calculator.classList.toggle("showing2");
+        if (!calculator.classList.contains("showing")) {
+          calculator.classList.toggle("showing");
         }
       }
       if (target.classList.contains("fa-calculator")) {
-        calculator.classList.toggle("showing2");
+        calculator.classList.toggle("showing");
 
+        if (!appCanvas.classList.contains("showing")) {
+          appCanvas.classList.toggle("showing");
+          button.classList.toggle("showing");
+        }
+      }
+      if (target.classList.contains("fa-keyboard")) {
+        typingGame.classList.toggle("showing");
+        timerCount();
         if (!appCanvas.classList.contains("showing")) {
           appCanvas.classList.toggle("showing");
           button.classList.toggle("showing");
---
+++ typing.js
@@ -0,0 +1,77 @@
+const typingGame = document.querySelector(".typingGame");
+const answer = document.querySelector(".answer");
+const question = document.querySelector(".question");
+const typingGameForm = document.querySelector(".typingGame form");
+const timer = document.querySelector(".timers");
+const score = document.querySelector(".scores");
+
+const aaa = [
+  "옛뿌리",
+  "섟",
+  "샛검불",
+  "널음새",
+  "불통거리다",
+  "어거리풍년",
+  "넌출",
+  "허릅숭이",
+  "새살거리다",
+  "벼리다",
+  "복성스럽다",
+  "보늬",
+  "덜퍽부리다",
+  "덧기둥",
+  "돌꼇잠",
+  "두바퀴굴림",
+];
+let pointScore = 0;
+let gameTimerSet = 10;
+let timerInterval;
+
+function timerCount() {
+  timerInterval = setInterval(gameTimer, 1000);
+}
+
+function resultGame(text) {
+  const random = Math.floor(Math.random() * aaa.length);
+  if (text == question.innerText) {
+    question.innerText = aaa[random];
+    pointScore++;
+    gameTimerSet += 1;
+    gameScore(pointScore);
+  }
+}
+
+function submitHandler(e) {
+  e.preventDefault();
+  const value = answer.value;
+  resultGame(value);
+  answer.value = "";
+}
+
+function gameOver() {
+  typingGame.innerHTML = `
+    <h1>Time ran out</h1>
+    <h4>your final score is ${pointScore}
+    <h4><button onclick="location.reload()">Reload</button>`;
+}
+
+function gameTimer() {
+  gameTimerSet--;
+  timer.innerText = gameTimerSet;
+  console.log(gameTimerSet);
+  if (gameTimerSet === 0) {
+    clearInterval(timerInterval);
+    gameOver();
+  }
+}
+
+function gameScore(value) {
+  score.innerText = value;
+}
+
+function init() {
+  typingGameForm.addEventListener("submit", submitHandler);
+  gameTimer();
+}
+
+init();
